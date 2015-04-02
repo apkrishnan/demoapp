c853abd18d05478f687fbab1724a76f840e6cf5d
Arun PK <akrishna@hach.com>
2015-04-02 04:13:06 -0600

    Added skeleton for proton-c messengerr

--- cloudRecv/main.cpp
+++ cloudRecv/main.cpp
@@ -8,21 +8,49 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-pn_message_t* message;
-pn_messenger_t* messenger;
+char* azurePathStr = (char*)"amqps://";
 
-#define check(messenger)                                                       \
-  do {                                                                         \
-     if(pn_messenger_errno(messenger))                                         \
-        die(__FILE__, __LINE__, pn_error_text(pn_messenger_error(messenger))); \
-  } while(0)                                                                   \
+pn_message_t* msgPtr;
+pn_messenger_t* messengerPtr;
 
-void die(const char* file, int line, const char* message) {
-    fprintf(stderr, "%s:%i: %s\n", file, line, message);
+#define check(messengerPtr)                                                       \
+  do {                                                                            \
+     if(pn_messenger_errno(messengerPtr))                                         \
+        die(__FILE__, __LINE__, pn_error_text(pn_messenger_error(messengerPtr))); \
+  } while(0)                                                                      \
+
+void die(const char* f_fileName, int f_line, const char* f_message) {
+    fprintf(stderr, "%s:%i: %s\n", f_fileName, f_line, f_message);
     exit(1);
 }
 
 int main(void) {
+
+	// Initiatlze message data pointer.
+	msgPtr = pn_message();
+
+	// Initialize and set messager to be non-blocking mode.
+	messengerPtr = pn_messenger(NULL);
+	pn_messenger_set_blocking(messengerPtr, false);
+
+	// Start the messenger.
+	pn_messenger_start(messengerPtr);
+	check(messengerPtr);
+
+	// Subscribe messenger to the Azure Service bus path.
+	pn_messenger_subscribe(messengerPtr, azurePathStr);
+	check(messengerPtr);
+
+	// Set to receive as many messages as messenger can buffer.
+	pn_messenger_recv(messengerPtr, -1);
+
+	// Main application loop.
+	while(1) {
+		// Block indefinitely until there has been socket activity.
+		pn_messenger_work(messengerPtr, -1);
+
+		// TODO: process the message.
+	}
 
 	return 0;
 }
