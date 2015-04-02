d1c0adcfbb36b2ed7ec7555709dc5a0f167eb545
Arun PK <akrishna@hach.com>
2015-04-02 06:58:32 -0600

    Implemented qpid message sending

--- cloudSend/main.cpp
+++ cloudSend/main.cpp
@@ -7,22 +7,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <time.h>
 
-pn_message_t* message;
-pn_messenger_t* messenger;
+char* azurePathStr = (char*)"amqps://owner:eHU40bVNQZHy8zOOErpbsROAo9cp5DjIl3yHgICQhL4=@arun-amqp-ns.servicebus.windows.net/arun-queue";
 
-#define check(messenger)                                                       \
-  do {                                                                         \
-     if(pn_messenger_errno(messenger))                                         \
-        die(__FILE__, __LINE__, pn_error_text(pn_messenger_error(messenger))); \
-  } while(0)                                                                   \
+pn_messenger_t* messengerPtr;
 
-void die(const char* file, int line, const char* message) {
-    fprintf(stderr, "%s:%i: %s\n", file, line, message);
+#define check(messengerPtr)                                                       \
+  do {                                                                            \
+     if(pn_messenger_errno(messengerPtr))                                         \
+        die(__FILE__, __LINE__, pn_error_text(pn_messenger_error(messengerPtr))); \
+  } while(0)                                                                      \
+
+void die(const char* f_fileName, int f_line, const char* f_message) {
+    fprintf(stderr, "%s:%i: %s\n", f_fileName, f_line, f_message);
     exit(1);
 }
 
 int main(void) {
+
+    // Initialize and start the messenger.
+    // Set to be non-blocking mode with outgoing window size of 1024.
+    messengerPtr = pn_messenger(NULL);
+    pn_messenger_set_blocking(messengerPtr, false);
+    pn_messenger_set_outgoing_window(messengerPtr, 1024); 
+    pn_messenger_start(messengerPtr);
+
+    while(1) {
+
+        // Wait for user key press.
+        getchar();
+
+        // Create JSON message string.
+        Json::Value valObj;
+        static const Json::StaticString sourceKey("Source");
+        valObj[sourceKey] = std::string("Beagle");
+
+        static const Json::StaticString timeKey("Time");
+        time_t t = time(NULL);
+        valObj[timeKey] = std::string(asctime(gmtime(&t)));
+
+        Json::FastWriter jsonWriter;
+        std::string jsonFile = jsonWriter.write(valObj);
+
+        printf("Sending message:\n%s\n", jsonFile.c_str());
+
+        // Initiatlze message data pointer.
+        pn_message_t* msgPtr = pn_message();
+        pn_message_set_address(msgPtr, azurePathStr);
+        pn_data_t* msgBodyPtr = pn_message_body(msgPtr);
+        pn_data_put_string(msgBodyPtr, pn_bytes(jsonFile.length(), jsonFile.c_str()));
+    
+        // Put new message into messenger.
+        pn_messenger_put(messengerPtr, msgPtr);
+        check(messengerPtr);
+
+        // Track the message send status.
+        pn_tracker_t msgTracker = pn_messenger_outgoing_tracker(messengerPtr);
+        pn_status_t msgStatus;
+        do {
+            pn_messenger_work(messengerPtr, -1);
+            msgStatus = pn_messenger_status(messengerPtr, msgTracker);
+        } while(msgStatus == PN_STATUS_PENDING);
+
+        // Free up message pointer.
+        pn_message_free(msgPtr);
+    }
 
     return 0;
 }
